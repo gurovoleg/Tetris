a9d38ffd1ea060574247e2330996a1adf2d5617f
gurovoleg <39372887+gurovoleg@users.noreply.github.com>
2020-02-24 20:24:25 +0300

    update sound function

--- docs/js/sound.js
+++ docs/js/sound.js
@@ -1,18 +1,24 @@
 let volumeEnabled = true
 
-const clearSound = new sound('sounds/cleared2.mp3')
-const levelSound = new sound('sounds/levelUp1.mp3')
-const moveSound = new sound('sounds/moved.wav')
-const rotateSound = new sound('sounds/rotate3.wav')
-const gameoverSound = new sound('sounds/welldone.ogg')
-const startSound = new sound('sounds/start1.ogg')
+// const clearSound = new sound('sounds/cleared2.mp3')
+// const levelSound = new sound('sounds/levelUp1.mp3')
+// const moveSound = new sound('sounds/moved.wav')
+// const rotateSound = new sound('sounds/rotate3.wav')
+// const gameoverSound = new sound('sounds/welldone.ogg')
+// const startSound = new sound('sounds/start1.ogg')
+
+const clearSound = new Audio('sounds/cleared2.mp3')
+const levelSound = new Audio('sounds/levelUp1.mp3')
+const moveSound = new Audio('sounds/moved.wav')
+const rotateSound = new Audio('sounds/rotate3.wav')
+const gameoverSound = new Audio('sounds/welldone.ogg')
+const startSound = new Audio('sounds/start1.ogg')
 
 
 function sound(src) {
   this.sound = document.createElement("audio")
   this.sound.src = src
   this.sound.setAttribute("preload", "auto")
-  this.sound.setAttribute("controls", "none")
   this.sound.style.display = "none"
   document.body.appendChild(this.sound)
   this.play = function(){
@@ -25,12 +31,12 @@ function sound(src) {
   }    
 }
 
-const sounds = {
-  clear: 'sounds/cleared2.mp3',
-  level: 'sounds/levelUp1.mp3',
-  move: 'sounds/moved.wav',
-  rotate: 'sounds/rotate3.wav',
-  applause: 'sounds/welldone.ogg',
-  start: 'sounds/start1.ogg',
-}
+// const sounds = {
+//   clear: 'sounds/cleared2.mp3',
+//   level: 'sounds/levelUp1.mp3',
+//   move: 'sounds/moved.wav',
+//   rotate: 'sounds/rotate3.wav',
+//   applause: 'sounds/welldone.ogg',
+//   start: 'sounds/start1.ogg',
+// }
 
